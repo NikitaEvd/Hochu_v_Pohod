c655894e13853c10bc3ad9b17396482038d0ac33
NikitaEvd <75009047+NikitaEvd@users.noreply.github.com>
2024-09-10 01:00:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,19 +4,33 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton, BotCommand, Inlin
 import logging
 import time
 
+# Логирование
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+# Получение токена
 TOKEN = os.environ.get('BOT_TOKEN')
+if TOKEN is None:
+    raise ValueError("Произошла ошибка: переменная окружения BOT_TOKEN не может быть 'None'")
+
 bot = telebot.TeleBot(TOKEN)
 
+# Чтение файла
 def read_items():
-    with open('hiking_items.txt', 'r', encoding='utf-8') as file:
-        return [line.strip() for line in file if line.strip()]
+    try:
+        with open('hiking_items.txt', 'r', encoding='utf-8') as file:
+            return [line.strip() for line in file if line.strip()]
+    except FileNotFoundError:
+        logger.error("Файл 'hiking_items.txt' не найден")
+        return []
+    except Exception as e:
+        logger.error(f"Ошибка при чтении файла: {e}")
+        return []
 
 user_progress = {}
 user_responses = {}
 
+# Функции создания клавиатур
 def get_start_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
     keyboard.add(KeyboardButton('Собраться в поход'), KeyboardButton('Посмотреть список'))
@@ -38,6 +52,7 @@ def reset_progress(user_id):
     user_progress[user_id] = 0
     user_responses[user_id] = {}
 
+# Хендлеры сообщений
 @bot.message_handler(commands=['start', 'reset'])
 def start(message):
     logger.info(f"Received start/reset command from user {message.from_user.id}")
@@ -65,9 +80,9 @@ def show_full_list(message):
 def ask_object(chat_id, user_id):
     items = read_items()
     current_object = user_progress.get(user_id, 0)
-    
+
     if current_object < len(items):
-        logger.info(f"Asking user {user_id} about item: {items[current_object]}")
+        logger.debug(f"Asking user {user_id} about item: {items[current_object]}")
         bot.send_message(chat_id, f"{items[current_object]}?", reply_markup=get_pack_keyboard())
     else:
         finish_packing(chat_id, user_id)
@@ -76,12 +91,12 @@ def ask_object(chat_id, user_id):
 def handle_response(message):
     user_id = message.from_user.id
     response = message.text.lower()
-    
+
     items = read_items()
     current_object = user_progress.get(user_id, 0)
-    
+
     if current_object < len(items):
-        logger.info(f"User {user_id} responded {response} to item {items[current_object]}")
+        logger.debug(f"User {user_id} responded {response} to item {items[current_object]}")
         user_responses.setdefault(user_id, {})[items[current_object]] = response
         user_progress[user_id] = current_object + 1
         ask_object(message.chat.id, user_id)
@@ -98,16 +113,16 @@ def finish_packing(chat_id, user_id):
 
 def show_lists(chat_id, user_id):
     responses = user_responses.get(user_id, {})
-    
-    packed = [item for item, status in responses.items() if status == 'Беру']
-    not_packed = [item for item, status in responses.items() if status == 'Возьму позже']
-    postponed = [item for item, status in responses.items() if status == 'В этот поход не буду брать']
-    
+
+    packed = [item for item, status in responses.items() if status == 'беру']
+    not_packed = [item for item, status in responses.items() if status == 'возьму позже']
+    postponed = [item for item, status in responses.items() if status == 'в этот поход не буду брать']
+
     result = "Вот, что получилось:\n\n"
     result += "Уже в рюкзаке:\n" + "\n".join(f"- {item}" for item in packed) + "\n\n"
     result += "Не забыть положить позже:\n" + "\n".join(f"- {item}" for item in not_packed) + "\n\n"
     result += "Не будете брать в этот поход:\n" + "\n".join(f"- {item}" for item in postponed)
-    
+
     bot.send_message(chat_id, result)
 
 @bot.callback_query_handler(func=lambda call: call.data == "edit_list")
@@ -119,26 +134,26 @@ def handle_edit_list(call):
         bot.send_message(call.message.chat.id, "У вас пока нет сохраненных ответов. Начните сбор заново.")
 
 def edit_list(message):
-    logger.info(f"Entered edit_list function for user {message.chat.id}")
+    logger.debug(f"Entered edit_list function for user {message.chat.id}")
     user_id = message.chat.id
     items = read_items()
     responses = user_responses.get(user_id, {})
-    
+
     if not responses:
         logger.info(f"No saved responses for user {user_id}")
         bot.send_message(message.chat.id, "У вас пока нет сохраненных ответов. Начните сбор заново.")
         return
-    
-    logger.info(f"Creating keyboard for item editing for user {user_id}")
+
+    logger.debug(f"Creating keyboard for item editing for user {user_id}")
     keyboard = InlineKeyboardMarkup(row_width=1)
     for item in items:
         status = responses.get(item, 'Не задано')
         callback_data = f"edit_{item[:45]}"
         keyboard.add(InlineKeyboardButton(f"{item} - {status}", callback_data=callback_data))
-    
+
     keyboard.add(InlineKeyboardButton("Назад", callback_data="back_to_final"))
-    
-    logger.info(f"Sending edit list message to user {user_id}")
+
+    logger.debug(f"Sending edit list message to user {user_id}")
     bot.edit_message_text("Выберите предмет для редактирования:", 
                           message.chat.id, 
                           message.message_id, 
@@ -149,16 +164,16 @@ def edit_item(call):
     logger.info(f"Received edit callback from user {call.from_user.id}")
     user_id = call.from_user.id
     item_prefix = call.data.split('_', 1)[1]
-    
+
     items = read_items()
     full_item = next((i for i in items if i.startswith(item_prefix)), item_prefix)
-    
+
     keyboard = InlineKeyboardMarkup(row_width=1)
     keyboard.add(InlineKeyboardButton("Да", callback_data=f"status_{full_item[:45]}_да"))
     keyboard.add(InlineKeyboardButton("Нет", callback_data=f"status_{full_item[:45]}_нет"))
     keyboard.add(InlineKeyboardButton("Отложить", callback_data=f"status_{full_item[:45]}_отложить"))
     keyboard.add(InlineKeyboardButton("Назад", callback_data="back_to_edit"))
-    
+
     bot.edit_message_text(f"Выберите статус для предмета '{full_item}':", 
                           call.message.chat.id, 
                           call.message.message_id, 
@@ -169,31 +184,31 @@ def set_status(call):
     logger.info(f"Received status callback from user {call.from_user.id}")
     user_id = call.from_user.id
     _, item_prefix, status = call.data.split('_')
-    
+
     items = read_items()
     full_item = next((i for i in items if i.startswith(item_prefix)), item_prefix)
-    
+
     user_responses.setdefault(user_id, {})[full_item] = status
-    
+
     bot.answer_callback_query(call.id, f"Статус обновлен: {status}")
-    
+
     edit_list_callback(call)
 
 @bot.callback_query_handler(func=lambda call: call.data == "back_to_edit")
 def edit_list_callback(call):
-    logger.info(f"Returning to edit list for user {call.from_user.id}")
+    logger.debug(f"Returning to edit list for user {call.from_user.id}")
     user_id = call.from_user.id
     items = read_items()
     responses = user_responses.get(user_id, {})
-    
+
     keyboard = InlineKeyboardMarkup(row_width=1)
     for item in items:
         status = responses.get(item, 'Не задано')
         callback_data = f"edit_{item[:45]}"
         keyboard.add(InlineKeyboardButton(f"{item} - {status}", callback_data=callback_data))
-    
+
     keyboard.add(InlineKeyboardButton("Назад", callback_data="back_to_final"))
-    
+
     bot.edit_message_text("Выберите предмет для редактирования:", 
                           call.message.chat.id, 
                           call.message.message_id, 
@@ -229,8 +244,8 @@ def show_full_list_after_packing(call):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    logger.info(f"Received message: '{message.text}' from user {message.from_user.id}")
-    bot.reply_to(message, "Извините, я не понимаю эту команду. Пожалуйста, используйте кнопки или команды /start и /reset.")
+    logger.debug(f"Received message: '{message.text}' from user {message.from_user.id}")
+    bot.reply_to(message, "Извините, я не понимаю эту команду. Пожалуйста, используйте /start или /reset.")
 
 def set_commands():
     bot.set_my_commands([
@@ -247,4 +262,4 @@ if __name__ == '__main__':
             bot.polling(none_stop=True)
         except Exception as e:
             logger.error(f"Bot crashed. Restarting. Error: {e}")
-            time.sleep(5)
+            time.sleep(10)
